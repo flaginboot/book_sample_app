d1bcc880b0230e7b4f65b09b30e04d7c4c6f6aec
Travis Wetmore <traviswetmore@gmail.com>
2011-06-28 20:18:32 -0400

    refactored routes

---
+++ test/integration/layout_links_test.rb
@@ -0,0 +1,26 @@
+require 'test_helper'
+
+class LayoutLinksTest < ActionDispatch::IntegrationTest
+  fixtures :all
+
+  test '"/" should be a Home page' do
+    get '/'
+    assert_select 'title', /Home/
+  end
+
+  test '"/contact" should be a Contact page' do
+    get '/contact'
+    assert_select 'title', /Contact/
+  end
+
+  test '"/about" should be a About page' do
+    get '/about'
+    assert_select 'title', /About/
+  end
+
+  test '"/help" should be a Help page' do
+    get '/help'
+    assert_select 'title', /Help/
+  end
+end
+
