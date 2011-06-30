7652b34bb1f16f6c79706a8b56170f5f72bc2d4e
Travis Wetmore <traviswetmore@gmail.com>
2011-06-29 20:22:52 -0400

    doing authentication

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
-  attr_accessible :name, :email
+  attr_accessor :password
+  attr_accessible :name, :email, :password, :password_confirmation
 
   email_regex = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
 
@@ -8,5 +9,25 @@ class User < ActiveRecord::Base
   validates :email, presence: true,
                     format: { with: email_regex },
                     uniqueness: { case_sensitive: false }
+  validates :password, presence: true,
+                       confirmation: true,
+                       length: { within: 6..40 }
+
+  before_save :encrypt_password
+
+  def self.authenticate(email, password)
+    user = find_by_email(email)
+    if user && BCrypt::Password.new(user.encrypted_password) == password
+      user
+    else
+      nil
+    end
+  end
+
+  def encrypt_password
+    if password.present?
+      self.encrypted_password = BCrypt::Password.create(self.password)
+    end
+  end
 end
 
--- test/unit/user_test.rb
+++ test/unit/user_test.rb
@@ -3,6 +3,8 @@ require 'test_helper'
 class UserTest < ActiveSupport::TestCase
   def setup
     @travis = users(:travis)
+    @travis.password = 'password'
+    @travis.password_confirmation = 'password'
   end
 
   test 'should create a valid User' do
@@ -10,25 +12,26 @@ class UserTest < ActiveSupport::TestCase
   end
 
   test 'should require a name' do
-    @travis.name = nil
-    refute @travis.valid?
+    refute users(:no_name).valid?
+    assert_equal ["can't be blank"], users(:no_name).errors[:name]
   end
 
   test 'should require a email address' do
-    @travis.email = nil
-    refute @travis.valid?
+    refute users(:no_email).valid?
+    assert_equal ["can't be blank", "is invalid"], users(:no_email).errors[:email]
   end
 
   test 'should reject names that are too long' do
-    @travis.name = 'a' * 51
-    refute @travis.valid?
+    refute users(:name_too_long).valid?
+    assert_equal ["is too long (maximum is 50 characters)"], users(:name_too_long).errors[:name]
   end
 
   test 'should only accept valid email addresses' do
-    addresses = %w[user@foo.com THE_USER@foo.bar.org first.last@foo.jp]
+    addresses = %w[user@foo.com THE_USER@foo.bar.org]
     addresses.each do |address|
       @travis.email = address
       assert @travis.valid?
+      assert_equal [], @travis.errors[:email]
     end
   end
 
@@ -37,15 +40,49 @@ class UserTest < ActiveSupport::TestCase
     addresses.each do |address|
       @travis.email = address
       refute @travis.valid?
+      assert_equal ["is invalid"], @travis.errors[:email]
     end
   end
 
   test 'should reject duplicate email addresses' do
-    refute users(:user_with_duplicated_email).valid?
+    refute users(:duplicated_email).valid?
+    assert_equal ["has already been taken"], users(:duplicated_email).errors[:email]
   end
 
   test 'should reject email addresses identical up to case' do
-    refute users(:user_with_duplicated_email_diff_case).valid?
+    refute users(:duplicated_email_diff_case).valid?
+    assert_equal ["has already been taken"], users(:duplicated_email_diff_case).errors[:email]
+  end
+
+  test 'should require a password' do
+    user = @travis
+    user.password = nil
+    refute user.valid?
+  end
+
+  test 'should require a matching password confirmation' do
+    user = @travis
+    user.password = 'passwerd'
+    user.password_confirmation = 'passwordy'
+    refute user.valid?
+  end
+
+  test 'should reject short passwords' do
+    user = @travis
+    user.password = 'pass'
+    user.password_confirmation = 'pass'
+    refute user.valid?
+  end
+
+  test 'should reject long passwords' do
+    user = @travis
+    user.password = 'a' * 41
+    user.password_confirmation = 'a' * 41
+    refute user.valid?
+  end
+
+  test 'should have a encrypted password' do
+    refute @travis.encrypted_password.blank?
   end
 end
 
