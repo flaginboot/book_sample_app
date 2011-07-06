b5f444b66e9165416dce34dafbe65d1c5d6a710f
Travis Wetmore <traviswetmore@gmail.com>
2011-07-06 17:19:50 -0400

    finished create action

--- test/functional/users_controller_test.rb
+++ test/functional/users_controller_test.rb
@@ -24,5 +24,22 @@ class UsersControllerTest < ActionController::TestCase
     get :show, id: @user
     assert_equal 'trav@wet.com', @user.email
   end
+
+  test 'form failure should not create user' do
+    assert_no_difference 'User.count' do
+      post :create, user: {  }
+    end
+  end
+
+#  test 'form failure should render #new page' do
+#    post :create, user: { name: nil }
+#    assert_redirected_to action: 'new'
+#  end
+
+  test 'create success should creat a user' do
+    assert_difference 'User.count', 1 do
+      post :create, user: { name: 'tttttt', email: 's@s.com', password: '1234567', password_confirmation: '1234567' }
+    end
+  end
 end
 
